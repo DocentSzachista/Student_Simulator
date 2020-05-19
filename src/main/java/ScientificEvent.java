9ef905f2394a72a2678548c2685f91ced1309eb5
Docent <idontknnow5325@gmail.com>
2020-05-19 10:19:29 +0200

    Wstępny zamysł na dodawanie ocen

--- src/main/java/ScientificEvent.java
+++ src/main/java/ScientificEvent.java
@@ -1,4 +1,5 @@
 public class ScientificEvent extends EventBase{
+    private Subject subject;
   public ScientificEvent(String description, StatisticType typeOfStatistic, int requirements ){
       this.description=description;
       this.typeOfStatistic=typeOfStatistic;
@@ -9,12 +10,20 @@ public class ScientificEvent extends EventBase{
     public void evaluateStudent(Student student) {
         if( student.getStatistic(typeOfStatistic)>requirements)
         {
-
+            switch(student.getStatistic(typeOfStatistic)-requirements){
+                case 1: subject.addMark(3);
+                case 2: subject.addMark(4);
+                case 3: subject.addMark(5);
+            }
         }
         else
         {
-
+            subject.addMark(2);
         }
 
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
     }
 }
